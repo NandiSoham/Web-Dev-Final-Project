aaeeb2343f3ce838982730f586278cca19ba1413
NandiSoham <nandisoham21@gmail.com>
2023-03-15 10:09:41 -0700

    developed work experience and project section

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,34 @@
-var button_menu = document.querySelector(".button");
-var menu = document.querySelector(".nav_list");
-var menuItems = document.querySelector("nav_links");
-button_menu.addEventListener("click", function () {
-  button_menu.classList.toggle("triggered");
-  menu.classList.toggle("triggered");
-});
-for (i = 0; i < menuItems.length; i++) {
-  menuItems[i].addEventListener("click", function () {
-    button_menu.classList.toggle("triggered");
+const nav = document.querySelector("nav");
+const button = document.querySelector(".button");
+const menu = document.querySelector(".nav_list");
+
+nav.addEventListener("click", (event) => {
+  const { target } = event;
+
+  if (target.classList.contains("button")) {
+    button.classList.toggle("triggered");
+    menu.classList.toggle("triggered");
+  } else if (target.classList.contains("menu")) {
+    button.classList.toggle("triggered");
     menu.classList.remove("triggered");
+  }
+});
+
+const tabs = document.querySelectorAll(".toggle_project");
+const contents = document.querySelectorAll(".content_project");
+
+function showTab(index) {
+  contents.forEach((content, i) => {
+    content.classList.toggle("is-active", i === index);
+  });
+  tabs.forEach((tab, i) => {
+    tab.classList.toggle("is-active", i === index);
   });
 }
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    showTab(index);
+  });
+});
+showTab(0);
